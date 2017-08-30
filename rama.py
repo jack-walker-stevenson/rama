5203e8763c89bc937903b41b6834e74a33c74bd0
Jack Stevenson <stevenson@college.harvard.edu>
2017-08-30 15:31:58 -0700

    moved vector operations into separate module. argparse is also working now: specify an optional --chain to process just one chain. also added verbose mode for fun

--- rama.py
+++ rama.py
@@ -1,92 +1,43 @@
-import operator
 import argparse
-from sys import argv
-from math import sqrt, atan2, degrees
+from sys import argv  # remove once argparse works
 
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB import is_aa
 from matplotlib import pyplot, rc
 
-
-# argparser = argparse.ArgumentParser()
-# argparser.add_argument('filename', type=str, nargs=1, help='pdb filename')
-# argparser.add_argument('chain', type=str, nargs=1, default='A', help='pdb filename')
-
-# pythonic vector ops are extensible to higher dimensions but slower here
-def add(v0, v1):
-    return v0[0] + v1[0], v0[1] + v1[1], v0[2] + v1[2]
+from vector_ops import torsion
 
 
-def add_pythonic(v0, v1):
-    return tuple(map(operator.add, v0, v1))
+argparser = argparse.ArgumentParser()
+argparser.add_argument('filename', type=str, help='name of pdb file')
+argparser.add_argument('-v', '--verbose', action='store_true',
+                       help='verbose mode')
+argparser.add_argument('--chain', type=str, help='specific chain to plot')
+args = argparser.parse_args()
 
-
-def subtract(v0, v1):
-    return v0[0] - v1[0], v0[1] - v1[1], v0[2] - v1[2]
-
-
-def subtract_pythonic(v0, v1):
-    return tuple(map(operator.sub, v0, v1))
-
-
-def normalize(v):
-    mag = sqrt(v[0] ** 2 + v[1] ** 2 + v[2] ** 2)
-    return v[0] / mag, v[1] / mag, v[2] / mag
-
-
-def normalize_pythonic(v):
-    mag = sqrt(sum([x ** 2 for x in v]))
-    return tuple([x / mag for x in v])
-
-
-def dot(v0, v1):
-    return v0[0] * v1[0] + v0[1] * v1[1] + v0[2] * v1[2]
-
-
-def dot_pythonic(v0, v1):
-    return sum([pair[0] * pair[1] for pair in zip(v0, v1)])
-
-
-def cross(v0, v1):
-    return (v0[1] * v1[2] - v0[2] * v1[1], -(v0[0] * v1[2] - v0[2] * v1[0]),
-            v0[0] * v1[1] - v0[1] * v1[0])
-
-
-def torsion(a, b, c, d):
-    # had to change to sub(a, b) from (b, a) to correct 180-degree rotation
-    # (probably wrong)
-    b1 = subtract(a, b)
-    b2 = subtract(b, c)
-    b3 = subtract(c, d)
-    n1 = normalize(cross(b1, b2))
-    n2 = normalize(cross(b2, b3))
-    m1 = cross(n1, normalize(b2))
-    x = dot(n1, n2)
-    y = dot(m1, n2)
-    return degrees(atan2(y, x))
-
-script, filename = argv
 # pull pdb id out of path string e.g. '../1axc.pdb'
-pdb_id = filename.split('/')[-1].split('.')[0].upper()
+pdb_id = args.filename.split('/')[-1].split('.')[0].upper()
 parser = PDBParser(PERMISSIVE=1)
-structure = parser.get_structure(pdb_id, filename)
+structure = parser.get_structure(pdb_id, args.filename)
+if args.verbose:
+    print("Verbose mode enabled. Parsing PDB {}.".format(pdb_id))
 phis = []
 psis = []
-"""
-# plot all residues
-for model in structure:
-    # using list(chain) to avoid weird residue indices
-    for lchain in [list(chain) for chain in model]:
+
+if args.chain:  # plot residues from a single chain
+    if args.verbose:
+        print("Using residues from chain {}.".format(args.chain))
+    for model in structure:
+        # using list() to avoid weird residue indices
+        lchain = list(model[args.chain])
         for i, res in enumerate(lchain):
             # next residue in chain for torsion calculation
-            nex = lchain[i+1]
+            n = lchain[i+1]
             # only calculate torsion if next residue is also an AA
-            if is_aa(nex):
+            if is_aa(n):
                 # make groups of relevant atoms to calculate angle
-                # phi: Ci, Ni+1, Ci+1alpha, Ci+1
-                # psi: Ni, Cialpha, Ci, Ni+1
-                phi_atoms = (res['C'], nex['N'], nex['CA'], nex['C'])
-                psi_atoms = (res['N'], res['CA'], res['C'], nex['N'])
+                phi_atoms = (res['C'], n['N'], n['CA'], n['C'])
+                psi_atoms = (res['N'], res['CA'], res['C'], n['N'])
                 # make group of coordinates of relevant atoms
                 phi_coords = (a.get_coord() for a in phi_atoms)
                 psi_coords = (a.get_coord() for a in psi_atoms)
@@ -98,35 +49,39 @@ for model in structure:
             # stop calculating torsions when AAs run out
             else:
                 break
-"""
-# plot chain A residues
-for model in structure:
-    # using list(chain) to avoid weird residue indices
-    lchain = list(model['A'])
-    for i, res in enumerate(lchain):
-        # next residue in chain for torsion calculation
-        n = lchain[i+1]
-        # only calculate torsion if next residue is also an AA
-        if is_aa(n):
-            # make groups of relevant atoms to calculate angle
-            phi_atoms = (res['C'], n['N'], n['CA'], n['C'])
-            psi_atoms = (res['N'], res['CA'], res['C'], n['N'])
-            # make group of coordinates of relevant atoms
-            phi_coords = (a.get_coord() for a in phi_atoms)
-            psi_coords = (a.get_coord() for a in psi_atoms)
-            # calculate torsions from groups of coordinates and store
-            phi = torsion(*phi_coords)
-            phis.append(phi)
-            psi = torsion(*psi_coords)
-            psis.append(psi)
-        # stop calculating torsions when AAs run out
-        else:
-            break
+else:  # plot residues from all chains
+    if args.verbose:
+        print("Using all residues.")
+    for model in structure:
+        # using list(chain) to avoid weird residue indices
+        for lchain in [list(chain) for chain in model]:
+            # if args.verbose: ...
+            for i, res in enumerate(lchain):
+                # next residue in chain for torsion calculation
+                nex = lchain[i+1]
+                # only calculate torsion if next residue is also an AA
+                if is_aa(nex):
+                    # make groups of relevant atoms to calculate angle
+                    # phi: Ci, Ni+1, Ci+1alpha, Ci+1
+                    # psi: Ni, Cialpha, Ci, Ni+1
+                    phi_atoms = (res['C'], nex['N'], nex['CA'], nex['C'])
+                    psi_atoms = (res['N'], res['CA'], res['C'], nex['N'])
+                    # make group of coordinates of relevant atoms
+                    phi_coords = (a.get_coord() for a in phi_atoms)
+                    psi_coords = (a.get_coord() for a in psi_atoms)
+                    # calculate torsions from groups of coordinates and store
+                    phi = torsion(*phi_coords)
+                    phis.append(phi)
+                    psi = torsion(*psi_coords)
+                    psis.append(psi)
+                # stop calculating torsions when AAs run out
+                else:
+                    break
 
 rc('text', usetex=True)
 pyplot.plot(phis, psis, 'bo')
 pyplot.axis([-180, 180, -180, 180])
-pyplot.gca().set_aspect('equal')
+pyplot.gca().set_aspect('equal')  # equal axis scale -> square plot
 pyplot.xticks([-180, -90, 0, 90, 180])
 pyplot.yticks([-180, -90, 0, 90, 180])
 pyplot.xlabel(r'$\phi$', size=14)
---
+++ vector_ops.py
@@ -0,0 +1,56 @@
+import operator
+from math import sqrt, atan2, degrees
+
+
+# pythonic vector ops are extensible to higher dimensions but slower here
+def add(v0, v1):
+    return v0[0] + v1[0], v0[1] + v1[1], v0[2] + v1[2]
+
+
+def add_pythonic(v0, v1):
+    return tuple(map(operator.add, v0, v1))
+
+
+def subtract(v0, v1):
+    return v0[0] - v1[0], v0[1] - v1[1], v0[2] - v1[2]
+
+
+def subtract_pythonic(v0, v1):
+    return tuple(map(operator.sub, v0, v1))
+
+
+def normalize(v):
+    mag = sqrt(v[0] ** 2 + v[1] ** 2 + v[2] ** 2)
+    return v[0] / mag, v[1] / mag, v[2] / mag
+
+
+def normalize_pythonic(v):
+    mag = sqrt(sum([x ** 2 for x in v]))
+    return tuple([x / mag for x in v])
+
+
+def dot(v0, v1):
+    return v0[0] * v1[0] + v0[1] * v1[1] + v0[2] * v1[2]
+
+
+def dot_pythonic(v0, v1):
+    return sum([pair[0] * pair[1] for pair in zip(v0, v1)])
+
+
+def cross(v0, v1):
+    return (v0[1] * v1[2] - v0[2] * v1[1], -(v0[0] * v1[2] - v0[2] * v1[0]),
+            v0[0] * v1[1] - v0[1] * v1[0])
+
+
+def torsion(a, b, c, d):
+    # had to change to sub(a, b) from (b, a) to correct 180-degree rotation
+    # (probably wrong)
+    b1 = subtract(a, b)
+    b2 = subtract(b, c)
+    b3 = subtract(c, d)
+    n1 = normalize(cross(b1, b2))
+    n2 = normalize(cross(b2, b3))
+    m1 = cross(n1, normalize(b2))
+    x = dot(n1, n2)
+    y = dot(m1, n2)
+    return degrees(atan2(y, x))
